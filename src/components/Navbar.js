0ed31d3fc3f739693a8aeb860313eaef1341d45c
nav-r-c <abhinav.30.kr@gmail.com>
2023-02-10 20:44:16 +0530

    Added props to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-function Navbar() {
+function Navbar(props) {
   return (
     <div>
         <nav className='flex justify-between bg-[#F5F5F5] py-2'>
             <div className='flex'>
-                <a href = "/"><h1 className='text-[2rem] px-5'>App Title</h1></a>
+                <a href = "/"><h1 className='text-[2rem] px-5'>{props.appTitle}</h1></a>
                 <ul className='flex items-center mx-2 text-[1rem]'>
                     <li className='mx-2'><a href = "/">Home</a></li>
                     <li className='mx-2'><a href = "/">Jobs</a></li>
@@ -17,6 +17,10 @@ function Navbar() {
         </nav>
     </div>
   )
+}
+
+Navbar.defaultProps = {
+    appTitle : "App Title"
 }
 
 export default Navbar
