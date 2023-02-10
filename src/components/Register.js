b625c9dfed3312e14b81a408f2de4283d17cf5f7
nav-r-c <abhinav.30.kr@gmail.com>
2023-02-10 23:26:10 +0530

    Added type field to form

--- src/components/Register.js
+++ src/components/Register.js
@@ -21,6 +21,17 @@ function Register() {
 				</div>
 
 				<div className='container my-5'>
+					<label className='text-[1.2rem] mx-5'>Type: </label>
+					<select className='px-5 py-2 border-2 rounded-lg bg-white'>
+						<option>Person</option>
+						<option>Inspector</option>
+						<option>Charity</option>
+						<option>Company</option>
+					</select>
+				</div>
+
+
+				<div className='container my-5'>
 					<label className='text-[1.2rem] mx-5'>Gender: </label>
 					<select className='px-5 py-2 border-2 rounded-lg bg-white'>
 						<option>Male</option>
