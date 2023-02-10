e7bcbf4c1a3f521bc701704110f1995f2437fcbc
nav-r-c <abhinav.30.kr@gmail.com>
2023-02-10 20:38:18 +0530

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import "./index.css"
 
 function App() {
-  return (
-    <div>App</div>
-  )
+	return (
+		<div>
+			<Navbar />
+		</div>
+	)
 }
 
 export default App
---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <div>
+        <nav className='flex justify-between bg-[#F5F5F5]'>
+            <div className='flex'>
+                <a href = "/"><h1 className='text-[2rem] px-5'>App Title</h1></a>
+                <ul className='flex items-center mx-2 text-[1rem]'>
+                    <li className='mx-2'><a href = "/">Home</a></li>
+                    <li className='mx-2'><a href = "/">Jobs</a></li>
+                    <li className='mx-2'><a href = "/">Charity</a></li>
+                    <li className='mx-2'><a href = "/">Insure Policies</a></li>
+                </ul>
+            </div>
+            
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
